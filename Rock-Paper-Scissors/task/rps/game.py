51ad6b21c34fad68dc5e15f43de3d161d7737aed
nlg734 <npsm111@gmail.com>
2020-05-18 09:27:06 -0500

    first commit, stage 1 complete

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,9 @@
+# Write your code here
+
+option = input()
+if option == "rock":
+    print("Sorry, but computer chose paper")
+if option == "scissors":
+    print("Sorry, but computer chose rock")
+if option == "paper":
+    print("Sorry, but computer chose scissors")
